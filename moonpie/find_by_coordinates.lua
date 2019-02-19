3917c4fde4ebad12aa78b9582914d6565bc2f10e
Trevor Redfern <trevorredfern@gmail.com>
2019-02-19 09:14:23 -0800

    Searches tree

--- moonpie/find_by_coordinates.lua
+++ moonpie/find_by_coordinates.lua
@@ -3,11 +3,20 @@
 -- This software is released under the MIT License.
 -- https://opensource.org/licenses/MIT
 
+local function search_by_coord(x, y, node, results)
+  if node.box:region():contains(x, y) then
+    results[#results + 1] = node
+
+    for _, v in ipairs(node.children) do
+      search_by_coord(x, y, v, results)
+    end
+  end
+end
 
 return function(x, y, tree)
   local r = {}
-  if tree.box:region():contains(x, y) then
-    r[#r + 1] = tree
-  end
+
+  search_by_coord(x, y, tree, r)
+
   return r
 end
