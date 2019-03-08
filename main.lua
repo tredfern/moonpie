2380629449f12fd2a819b760d6cb056a237d39d0
Trevor Redfern <trevorredfern@gmail.com>
2019-03-08 08:18:53 -0800

    Cleaned up button demo

--- main.lua
+++ main.lua
@@ -81,30 +81,22 @@ local function button_layout()
   return {
     header("Buttons"),
     {
-      components.button_group({
-        margin = 5,
-        buttons = {
-          components.button({ caption = "Default" }),
-          components.button({ style = "button_primary", caption = "Primary" }),
-          components.button({ style = "button_info", caption = "Info" }),
-          components.button({ style = "button_warning", caption = "Warning" }),
-          components.button({ style = "button_success", caption = "Success" }),
-          components.button({ style = "button_danger", caption = "Danger" }),
-        }
-      }),
+      margin = 5,
+      components.button({ caption = "Default" }),
+      components.button({ style = "button_primary", caption = "Primary" }),
+      components.button({ style = "button_info", caption = "Info" }),
+      components.button({ style = "button_warning", caption = "Warning" }),
+      components.button({ style = "button_success", caption = "Success" }),
+      components.button({ style = "button_danger", caption = "Danger" }),
     },
     {
-      components.button_group({
-        margin = 5,
-        buttons = {
-          components.button({ style = "button_small", caption = "Default" }),
-          components.button({ style = "button_primary button_small", caption = "Primary" }),
-          components.button({ style = "button_info button_small", caption = "Info" }),
-          components.button({ style = "button_warning button_small", caption = "Warning" }),
-          components.button({ style = "button_success button_small", caption = "Success" }),
-          components.button({ style = "button_danger button_small", caption = "Danger" }),
-        }
-      }),
+      margin = 5,
+      components.button({ style = "button_small", caption = "Default" }),
+      components.button({ style = "button_primary button_small", caption = "Primary" }),
+      components.button({ style = "button_info button_small", caption = "Info" }),
+      components.button({ style = "button_warning button_small", caption = "Warning" }),
+      components.button({ style = "button_success button_small", caption = "Success" }),
+      components.button({ style = "button_danger button_small", caption = "Danger" }),
     }
   }
 end
