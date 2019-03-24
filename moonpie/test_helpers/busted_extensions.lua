459a08fcad521b1577ee43b2962f8afbd59aba2d
Trevor Redfern <trevorredfern@gmail.com>
2019-03-23 21:05:17 -0700

    Fixing build by ensuring unpack is configured properly

--- moonpie/test_helpers/busted_extensions.lua
+++ moonpie/test_helpers/busted_extensions.lua
@@ -3,5 +3,6 @@
 -- This software is released under the MIT License.
 -- https://opensource.org/licenses/MIT
 
+require "moonpie.test_helpers.ensure_unpack"
 require "moonpie.test_helpers.array_assertions"
 require "moonpie.test_helpers.mock_love"
---
+++ moonpie/test_helpers/ensure_unpack.lua
@@ -0,0 +1,8 @@
+-- Copyright (c) 2019 Redfern, Trevor <trevorredfern@gmail.com>
+--
+-- This software is released under the MIT License.
+-- https://opensource.org/licenses/MIT
+
+if table.unpack == nil then
+  table.unpack = unpack
+end
