afda0e86953c308db402132ae7cf777a912efadc
Trevor Redfern <trevorredfern@gmail.com>
2020-02-11 20:49:29 -0500

    Expanded dice library to maybe functional

--- moonpie/math/dice.lua
+++ moonpie/math/dice.lua
@@ -3,12 +3,21 @@
 -- This software is released under the MIT License.
 -- https://opensource.org/licenses/MIT
 
+local cup = { modifier = 0 }
+function cup:roll()
+  local sum = 0
+  for _, v in ipairs(self) do
+    sum = sum + v()
+  end
+  return sum + self.modifier
+end
+
 local die = {}
 
 function die.new(sides)
   local d = {}
   d.sides = sides
-  setmetatable(d, { __call = die.roll })
+  setmetatable(d, { __call = die.roll, __tostring = die.string })
   return d
 end
 
@@ -16,8 +25,41 @@ function die.roll(self)
   return math.random(1, self.sides)
 end
 
+function die.string(self)
+  return string.format("d%d", self.sides)
+end
+
 local dice = {
-  d6 = die.new(6)
+  d3 = die.new(3),
+  d4 = die.new(4),
+  d6 = die.new(6),
+  d8 = die.new(8),
+  d10 = die.new(10),
+  d12 = die.new(12),
+  d20 = die.new(20),
+  d100 = die.new(100),
 }
+
+function dice.cup(...)
+  local c = {...}
+  setmetatable(c, { __index = cup, __call = cup.roll })
+  return c
+end
+
+function dice.parse(str)
+  local c = {}
+  string.gsub(str, "(%d)d(%d)", function(count, sides)
+    for _=1,count do
+      c[#c +1] = die.new(sides)
+    end
+  end)
+  string.gsub(str, "([-+]%d)$", function(mod)
+    c.modifier = mod
+  end)
+
+  setmetatable(c, { __index = cup, __call = cup.roll })
+  return c
+end
+
 
 return dice
--- moonpie/math/dice_spec.lua
+++ moonpie/math/dice_spec.lua
@@ -6,10 +6,52 @@
 describe("moonpie.math.dice", function()
   local dice = require "moonpie.math.dice"
 
-  it("can roll a d6 die", function()
+  it("can roll some dice", function()
     for _=1,100 do
-      local result = dice.d6()
-      assert.in_range(1, 6, result)
+      assert.in_range(1, 3, dice.d3())
+      assert.in_range(1, 4, dice.d4())
+      assert.in_range(1, 6, dice.d6())
+      assert.in_range(1, 8, dice.d8())
+      assert.in_range(1, 10, dice.d10())
+      assert.in_range(1, 12, dice.d12())
+      assert.in_range(1, 20, dice.d20())
+      assert.in_range(1, 100, dice.d100())
+    end
+  end)
+
+  it("can roll multiple dice together", function()
+    local cup = dice.cup(dice.d6, dice.d6, dice.d3, dice.d20)
+    for _=1,100 do
+      assert.in_range(4, 35, cup())
+    end
+  end)
+
+  it("an make a cup with a modifier", function()
+    local cup = dice.cup(dice.d6, dice.d6)
+    cup.modifier = 2
+    for _=1,100 do
+      assert.in_range(4, 14, cup())
+    end
+  end)
+
+  it("Can turn dice into names", function()
+    assert.equals("d4", tostring(dice.d4))
+    assert.equals("d6", tostring(dice.d6))
+    assert.equals("d8", tostring(dice.d8))
+    assert.equals("d10", tostring(dice.d10))
+    assert.equals("d12", tostring(dice.d12))
+    assert.equals("d20", tostring(dice.d20))
+    assert.equals("d100", tostring(dice.d100))
+  end)
+
+  it("can parse a string of dice into a cup", function()
+    local cup = dice.parse("2d6+2")
+    local cup2 = dice.parse("2d6-2")
+    local cup3 = dice.parse("2d6+2d8+2")
+    for _=1, 100 do
+      assert.in_range(4, 14, cup())
+      assert.in_range(0, 10, cup2())
+      assert.in_range(6, 30, cup3())
     end
   end)
 end)
