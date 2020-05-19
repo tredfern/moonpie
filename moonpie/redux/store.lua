79ea56181861d6f8405e07c2ae45767d3a3bcac9
Trevor Redfern <trevorredfern@gmail.com>
2020-05-19 07:29:47 -0400

    Refactored store into a singleton

--- moonpie/redux/store.lua
+++ moonpie/redux/store.lua
@@ -3,44 +3,49 @@
 -- This software is released under the MIT License.
 -- https://opensource.org/licenses/MIT
 
-local class = require "moonpie.class"
-local store = {}
+local store = { }
+local reducer_handler
+local listeners = setmetatable({}, { __mode = "v" })
+local state = {}
 
-function store:constructor(reducer, initial_state)
-  assert(reducer, "Store requires a reducer function")
-  self.reducer = reducer
-  self.listeners = setmetatable({}, { __mode = "v" })
-  self.state = initial_state
-end
-
-function store:dispatch(action, bypass_trigger)
-  -- move to middleware?
-  if type(action) == "function" then
-    action(
-      function(__action) self:dispatch(__action, true) end,
-      function() return self:get_state() end
-    )
-    self:trigger_listeners()
-  else
-    self.state = self.reducer(self.state, action)
-    if not bypass_trigger then
-      self:trigger_listeners()
-    end
-  end
-end
-
-function store:subscribe(listener)
-  self.listeners[#self.listeners + 1] = listener
-end
-
-function store:trigger_listeners()
-  for _, v in ipairs(self.listeners) do
+local function trigger_listeners()
+  for _, v in ipairs(listeners) do
     v()
   end
 end
 
-function store:get_state()
-  return self.state
+function store.create_store(reducer, initial_state)
+  assert(reducer, "Store requires a reducer function")
+  reducer_handler = reducer
+  state = initial_state
 end
 
-return class(store)
+function store.dispatch(action, bypass_trigger)
+  -- move to middleware?
+  if type(action) == "function" then
+    action(
+      function(__action) store.dispatch(__action, true) end,
+      store.get_state
+    )
+    trigger_listeners()
+  else
+    state = reducer_handler(state, action)
+    if not bypass_trigger then
+      trigger_listeners()
+    end
+  end
+end
+
+function store.subscribe(listener)
+  listeners[#listeners + 1] = listener
+end
+
+function store.get_state()
+  return state
+end
+
+function store.get_listeners()
+  return listeners
+end
+
+return store
--- moonpie/redux/store_spec.lua
+++ moonpie/redux/store_spec.lua
@@ -8,11 +8,12 @@ describe("moonpie.redux.store", function()
 
   it("can have actions dispatched that are called into the reducer", function()
     local reducer = spy.new(function() end)
-    local s = store:new(reducer)
+    store.create_store(reducer)
 
     local action = {}
-    s:dispatch(action)
-    assert.spy(reducer).was.called_with(s.state, action)
+    local state = store.get_state()
+    store.dispatch(action)
+    assert.spy(reducer).was.called_with(state, action)
   end)
 
   it("calls listeners when state has changed", function()
@@ -20,42 +21,42 @@ describe("moonpie.redux.store", function()
     local listener = spy.new(function() end)
     local listener2 = spy.new(function() end)
 
-    local s = store:new(reducer)
-    s:subscribe(listener)
-    s:subscribe(listener2)
-    s:dispatch({})
+    store.create_store(reducer)
+    store.subscribe(listener)
+    store.subscribe(listener2)
+    store.dispatch({})
     assert.spy(listener).was.called()
     assert.spy(listener2).was.called()
   end)
 
   it("listeners are weak values and allow garbage collection to occur", function()
-    local s = store:new(function() end)
+    store.create_store(function() end)
     local listener = function() end
-    s:subscribe(listener)
+    store.subscribe(listener)
     listener = nil
     assert.is_nil(listener) -- kind of annoying workaround for luacheck error
     collectgarbage()
-    assert.equals(0, #s.listeners)
+    assert.equals(0, #store.get_listeners())
   end)
 
   it("provides access to the current version of state", function()
     local reduce_state = {}
     local initial_state = {}
-    local s = store:new(function() return reduce_state end, initial_state)
-    assert.equals(initial_state, s:get_state())
-    s:dispatch({})
-    assert.equals(reduce_state, s:get_state())
+    store.create_store(function() return reduce_state end, initial_state)
+    assert.equals(initial_state, store.get_state())
+    store.dispatch({})
+    assert.equals(reduce_state, store.get_state())
   end)
 
   it("passes the current state into the reducer", function()
     local state = {}
     local reducer = spy.new(function() return {} end)
-    local s = store:new(reducer, state)
+    store.create_store(reducer, state)
     local action = {}
-    s:dispatch(action)
+    store.dispatch(action)
     assert.spy(reducer).was.called_with(action, state)
-    state = s:get_state()
-    s:dispatch(action)
+    state = store.get_state()
+    store.dispatch(action)
     assert.spy(reducer).was.called_with(action, state)
   end)
 
@@ -78,10 +79,10 @@ describe("moonpie.redux.store", function()
       return state
     end
 
-    local s = store:new(reducer)
-    s:dispatch(complex())
-    assert.is_true(s:get_state().nice)
-    assert.is_false(s:get_state().in_conversation)
+    store.create_store(reducer)
+    store.dispatch(complex())
+    assert.is_true(store.get_state().nice)
+    assert.is_false(store.get_state().in_conversation)
   end)
 
   it("dispatches only one listener call when dispatching a function", function()
@@ -95,9 +96,9 @@ describe("moonpie.redux.store", function()
 
     local reducer = function() return {} end
     local listener = spy.new(function() end)
-    local s = store:new(reducer)
-    s:subscribe(listener)
-    s:dispatch(action_group())
+    store.create_store(reducer)
+    store.subscribe(listener)
+    store.dispatch(action_group())
     assert.equals(1, #listener.calls)
   end)
 end)
