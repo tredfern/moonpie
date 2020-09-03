19a15457a27882fb545a749cb5b9f49c035df255
Trevor Redfern <trevorredfern@gmail.com>
2020-09-02 21:24:54 -0400

    Simplified event_system

--- moonpie/event_system.lua
+++ moonpie/event_system.lua
@@ -8,23 +8,17 @@ local callback = require "moonpie.callback"
 local event_system = {}
 local events = {}
 
-function event_system.register(event_name)
-  events[event_name] = callback:new()
-end
-
-function event_system.is_registered(event_name)
-  return events[event_name] ~= nil
-end
-
 function event_system.subscribe(event_name, handler)
+  if not events[event_name] then
+    events[event_name] = callback:new()
+  end
   events[event_name]:add(handler)
 end
 
 function event_system.dispatch(event)
-  if not events[event.type] then
-    error(string.format("No event registered for %s", event.type))
+  if events[event.type] then
+    events[event.type]:trigger(event)
   end
-  events[event.type]:trigger(event)
 end
 
 
--- moonpie/event_system_spec.lua
+++ moonpie/event_system_spec.lua
@@ -6,19 +6,8 @@
 describe("moonpie.utility.event_system", function()
   local event_system = require "moonpie.event_system"
 
-  it("can have events registered", function()
-    event_system.register("my_event")
-    assert.is_true(event_system.is_registered("my_event"))
-  end)
-
-  it("can have consumers subscribe to events", function()
-    event_system.register("my_event")
-    event_system.subscribe("my_event", function() end)
-  end)
-
   it("dispatch events", function()
     local s = spy.new(function() end)
-    event_system.register("my_event")
     event_system.subscribe("my_event", spy_to_func(s))
 
     local event = { type = "my_event" }
@@ -27,9 +16,8 @@ describe("moonpie.utility.event_system", function()
   end)
 
   it("raises a clear error if the event triggered has no queue", function()
-    assert.has_error(function()
+    assert.has_no_error(function()
       event_system.dispatch({ type = "missing_event" })
-    end,
-    "No event registered for missing_event")
+    end)
   end)
 end)
