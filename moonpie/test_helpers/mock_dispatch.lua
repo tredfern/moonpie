0a4bd7ad54e56484de0e65c33d3f3af5cf0bb6c8
Trevor Redfern <trevorredfern@gmail.com>
2021-05-08 11:31:50 -0400

    Moved mockDispatch to moonpie

---
+++ moonpie/test_helpers/mock_dispatch.lua
@@ -0,0 +1,35 @@
+local tables = require "moonpie.tables"
+
+local mockDispatch = setmetatable({
+
+  dispatched = {},
+  processComplex = false,
+  received_action = function(self, action)
+    if action == nil then
+      print("searching for nil action in mock dispatch")
+    end
+    return tables.any(self.dispatched, function(d) return d.type == action end)
+  end,
+  reset = function(self)
+    self.dispatched = {}
+    self.processComplex = false
+  end
+}, {
+  __call = function(self, action)
+    if type(action) == "function" then
+      if self.processComplex then
+        local store = require "moonpie.redux.store"
+        action(self, store.getState)
+      end
+      return
+    end
+    self.dispatched[#self.dispatched + 1] = action
+  end
+})
+
+mockDispatch.asFunction = function(...)
+  return mockDispatch(...)
+end
+
+
+return mockDispatch
