15ea79e56dafa3db8c0863c81753b1ea4dbb62d9
Trevor Redfern <trevorredfern@gmail.com>
2021-05-08 21:42:26 -0400

    Got all mockDispatch changes from Terminus

--- moonpie/test_helpers/mock_dispatch.lua
+++ moonpie/test_helpers/mock_dispatch.lua
@@ -1,14 +1,25 @@
+-- Copyright (c) 2021 Trevor Redfern
+--
+-- This software is released under the MIT License.
+-- https://opensource.org/licenses/MIT
+
 local tables = require "moonpie.tables"
+local isCallable = require "moonpie.utility.is_callable"
 
 local mockDispatch = setmetatable({
 
   dispatched = {},
   processComplex = false,
-  received_action = function(self, action)
+  received_action = function(self, action, count)
     if action == nil then
       print("searching for nil action in mock dispatch")
     end
-    return tables.any(self.dispatched, function(d) return d.type == action end)
+    if count then
+      local list = tables.select(self.dispatched, function(d) return d.type == action end)
+      return list and count == #list
+    else
+      return tables.any(self.dispatched, function(d) return d.type == action end)
+    end
   end,
   reset = function(self)
     self.dispatched = {}
@@ -16,14 +27,16 @@ local mockDispatch = setmetatable({
   end
 }, {
   __call = function(self, action)
-    if type(action) == "function" then
+    if isCallable(action) then
       if self.processComplex then
         local store = require "moonpie.redux.store"
         action(self, store.getState)
       end
-      return
     end
-    self.dispatched[#self.dispatched + 1] = action
+
+    if type(action) == "table" then
+      self.dispatched[#self.dispatched + 1] = action
+    end
   end
 })
 
