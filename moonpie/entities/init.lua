322f20b941d53215c5f3d03b53bdb652d81ac9c3
tredfern <trevorredfern@gmail.com>
2021-12-14 21:17:49 -0500

    create entity module

---
+++ moonpie/entities/init.lua
@@ -0,0 +1,12 @@
+-- Copyright (c) 2021 Trevor Redfern
+--
+-- This software is released under the MIT License.
+-- https://opensource.org/licenses/MIT
+
+return {
+  actions = require "moonpie.entities.actions",
+  entity = require "moonpie.entities.entity",
+  property = require "moonpie.entities.property",
+  reducer = require "moonpie.entities.reducer",
+  selectors = require "moonpie.entities.selectors"
+}
